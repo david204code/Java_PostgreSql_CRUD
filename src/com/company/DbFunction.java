232c47ba3e42be42df0ea0fb94432f56fe02fbe9
David Wu <34573725+david204code@users.noreply.github.com>
2022-12-06 16:52:53 +0000

    Inserted record onto the database, to the employee table

--- src/com/company/DbFunction.java
+++ src/com/company/DbFunction.java
@@ -39,4 +39,17 @@ public class DbFunction {
             System.out.println(e);
         }
     }
+
+    public void insert_row(Connection conn, String table_name, String name, String address) {
+        Statement statement;
+
+        try {
+            String query = String.format("insert into %s(name, address) values('%s', '%s'); ", table_name, name, address);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row Inserted");
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
 }
--- src/com/company/main.java
+++ src/com/company/main.java
@@ -6,6 +6,7 @@ public class main {
     //An instance of the class db
         DbFunction db = new DbFunction();
         Connection conn = db.connect_to_db("tutdb", "postgres", "password");
-        db.createTable(conn, "employee");
+//        db.createTable(conn, "employee");
+        db.insert_row(conn, "employee", "Jose", "England");
     }
 }
