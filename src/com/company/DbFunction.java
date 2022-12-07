defb097c8f7978f49679c4395ee32fac02665fe4
David Wu <34573725+david204code@users.noreply.github.com>
2022-12-07 20:08:56 +0000

    Query the database: add the function to search by employee id

--- src/com/company/DbFunction.java
+++ src/com/company/DbFunction.java
@@ -104,4 +104,23 @@ public class DbFunction {
             System.out.println(e);
         }
     }
+
+    public void search_by_id(Connection conn, String table_name, int id) {
+        Statement statement;
+        //Need ResultSet because output will be returned
+        ResultSet rs = null;
+
+        try {
+            String query = String.format("select * from %s where empid = %s", table_name, id);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.print(rs.getString("empid") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.print(rs.getString("address"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- src/com/company/main.java
+++ src/com/company/main.java
@@ -9,7 +9,8 @@ public class main {
 //        db.createTable(conn, "employee");
 //        db.insert_row(conn, "employee", "Jose", "England");
 //        db.update_name(conn, "employee", "Jose", "Matthew");
-        db.read_data(conn, "employee");
-        db.search_by_name(conn, "employee",  "Matthew");
+//        db.read_data(conn, "employee");
+//        db.search_by_name(conn, "employee",  "Matthew");
+        db.search_by_id(conn, "employee", 4);
     }
 }
